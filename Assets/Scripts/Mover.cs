52370012a949bf34868e54b4609dad2a7a87eee3
drMEathead225 <hollow269@morrisville.edu>
2026-09-09 00:00:44 -0400

    LAB_01 Changes

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    void Start()
+    {
+        PrintInstruction();
+    }
+
+     void PrintInstruction()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move using arrow keys or wasd");
+        Debug.Log("Don't bump into objects!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue, yValue, zValue); 
+    }
+    
+    
+}
