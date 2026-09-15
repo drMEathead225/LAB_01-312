266b0e1041335b502e1e16d2a375476e8c0cd76e
drMEathead225 <hollow269@morrisville.edu>
2026-09-14 21:31:41 -0400

    gg

--- Assets/Scripts/ObjectHit.cs
+++ Assets/Scripts/ObjectHit.cs
@@ -5,8 +5,11 @@ public class ObjectHit : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnCollisionEnter(Collision other)
     {
-        GetComponent<MeshRenderer>().material.color = Color.red;
-        Debug.Log ("Something hit me!");
+        if (other.gameObject.tag != "Player")
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+            gameObject.tag = "Hit";
+        }
     }
 
     // Update is called once per frame
