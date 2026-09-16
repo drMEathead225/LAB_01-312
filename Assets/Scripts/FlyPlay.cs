b9238f52851cdc7de9cb57d74a07fa5a3120997f
drMEathead225 <hollow269@morrisville.edu>
2026-09-15 23:49:58 -0400

    2.18, 2.19, 2.20

---
+++ Assets/Scripts/FlyPlay.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class FlyPlay : MonoBehaviour
+{
+    [SerializeField] float speed = 1.0f;
+    [SerializeField] Transform player;
+    Vector3 playerPosition;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerPosition = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, 0.01f);
+    }
+}
